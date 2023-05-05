ba89ed5caf7f37c74c470ed142e6109cdf44c564
Alex Berthon <Alexjberthon@gmail.com>
2023-05-05 16:23:42 -0700

    Highscore UI Created UI elements to house highscore table, new highscore panel, restart/exit buttons, etc.
    necessary UI elements to add the highscore feature

---
+++ Highscore.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake {
+    internal class Highscore {
+        String username;
+        int score;
+
+        public Highscore(String name, int score) {
+            this.username = name;
+            this.score = score;
+        }
+
+        public int getScore() {
+            return score;
+        }
+
+        public String getName() {
+            return username;
+        }
+
+        private class SortScoreAcendingHelper : IComparer {
+            int IComparer.Compare(object a, object b) {
+                Highscore s1 = (Highscore)a;
+                Highscore s2 = (Highscore)b;
+
+                if (s1.score < s2.score) {
+                    return 1;
+                }
+
+                if (s1.score > s2.score) {
+                    return -1;
+                }
+                else {
+                    return 0;
+                }
+            }
+        }
+
+        public static IComparer SortScoreAcending() {
+            return (IComparer)new SortScoreAcendingHelper();
+        }
+    }
+}
